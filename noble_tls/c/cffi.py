2b08414e21b1bf38ca5e29dc599a0431d7ec60a5
liuxiaotong05 <liuxiaotong05@kuaishou.com>
2025-09-29 20:34:51 +0800

    fix load asset

--- noble_tls/c/cffi.py
+++ noble_tls/c/cffi.py
@@ -53,10 +53,15 @@ def load_asset():
 
     asset_name = generate_asset_name(version=current_version)
     asset_path = f'{root_dir()}/dependencies/{asset_name}'
-    if not os.path.exists(asset_path):
-        raise TLSClientException(f"Unable to find asset {asset_name} for version {current_version}.")
+    if os.path.exists(asset_path):
+        return asset_name
 
-    return asset_name
+    asset_name_xgo = generate_asset_name(version=current_version, with_xgo=True)
+    asset_path = f'{root_dir()}/dependencies/{asset_name_xgo}'
+    if os.path.exists(asset_path):
+        return asset_name_xgo
+
+    raise TLSClientException(f"Unable to find asset {asset_name} or {asset_name_xgo} for version {current_version}.")
 
 
 def initialize_library():
