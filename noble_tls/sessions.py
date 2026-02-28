7d2e1f2c702d0e376962342bf780e5933fec61dc
liuxiaotong05 <liuxiaotong05@kuaishou.com>
2026-02-28 15:35:50 +0800

    do not modify self.headers in execute_request

--- noble_tls/sessions.py
+++ noble_tls/sessions.py
@@ -325,8 +325,7 @@ class Session:
         else:
             request_body = data
             content_type = None
-        # set content type if it isn't set
-        # --- Headers --------------------------------------------------------------------------------------------------
+
         if self.headers is None:
             headers = CaseInsensitiveDict(headers)
         elif headers is None:
